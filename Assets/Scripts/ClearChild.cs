00aeb5ad8c88364829e876fcc61e15bd4d890c24
Nat <nathanaelangelotecson@gmai;.com>
2024-11-23 19:58:21 +0800

    commit

---
+++ Assets/Scripts/ClearChild.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ClearChild : MonoBehaviour
+{
+    #region Public Functions
+    public void ClearChildren()
+    {
+        int _i = 0;
+        GameObject[] _allChildren = new GameObject[transform.childCount];
+        foreach (Transform _child in transform)
+        {
+            _allChildren[_i] = _child.gameObject;
+            _i += 1;
+        }
+        foreach (GameObject _child in _allChildren)
+            Destroy(_child.gameObject);
+    }
+    #endregion
+}
