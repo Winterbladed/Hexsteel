81b069f1e93397168903af58eb83f64a79493e52
Nat <nathanaelangelotecson@gmai;.com>
2024-11-27 15:02:30 +0800

    commit

---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Throwable : Damage
+{
+    #region Variables
+    [Header("Throwable Extras")]
+    [SerializeField] private GameObject _throwable;
+    private float _throwTime;
+    #endregion
+
+    #region Private Functions
+
+    #endregion
+}
