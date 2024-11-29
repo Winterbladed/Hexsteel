c7c73d9f347c35c4bdb5dd0461f4661ec7dbd1d2
Nat <nathanaelangelotecson@gmai;.com>
2024-11-30 01:30:48 +0800

    commit

--- Assets/Scripts/Feet.cs
+++ Assets/Scripts/Feet.cs
@@ -7,61 +7,32 @@ public class Feet : MonoBehaviour
 
     [Header("Movement Sound Library")]
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private AudioSource _audioSourcer;
-    [SerializeField] private AudioClip[] _audioClip;
+    [SerializeField] private AudioClip _audioClip;
     private float _movementStep = 0.0f;
-    private bool _isSoft, _isWater, _isHard;
     #endregion
 
     #region Private Functions
     private void Update()
     {
-        if (_movement.GetIsWalking() && !_movement.GetIsRunning() && !_movement.GetIsAttacking() && !_movement.GetIsShooting() && !_movement.GetIsDodging())
+        if (_movement.GetIsWalking() && !_movement.GetIsRunning() && !_movement.GetIsAttacking() && !_movement.GetIsShooting() && !_movement.GetIsDodging() && !_movement.GetIsThrowing() && _movement.GetIsGrounded())
         {
             _movementStep += Time.deltaTime;
             if (_movementStep > 0.5f)
             {
-                if (_isWater) _audioSourcer.PlayOneShot(_audioClip[0]);
-                else if (_isHard && !_isWater) _audioSource.PlayOneShot(_audioClip[2]);
-                else if (_isSoft && !_isWater && !_isHard) _audioSource.PlayOneShot(_audioClip[1]);
+                _audioSource.PlayOneShot(_audioClip);
                 _movementStep = 0.0f;
             }
         }
-        else if (_movement.GetIsRunning() && !_movement.GetIsAttacking() && !_movement.GetIsShooting() && !_movement.GetIsDodging())
+        else if (_movement.GetIsRunning() && !_movement.GetIsAttacking() && !_movement.GetIsShooting() && !_movement.GetIsDodging() && !_movement.GetIsThrowing() && _movement.GetIsGrounded())
         {
             _movementStep += Time.deltaTime;
             if (_movementStep > 0.25f)
             {
-                if (_isWater) _audioSourcer.PlayOneShot(_audioClip[0]);
-                else if (_isHard && !_isWater) _audioSource.PlayOneShot(_audioClip[2]);
-                else if (_isSoft && !_isWater && !_isHard) _audioSource.PlayOneShot(_audioClip[1]);
+                _audioSource.PlayOneShot(_audioClip);
                 _movementStep = 0.0f;
             }
         }
-        else
-            _movementStep = 1.0f;
-    }
-    #endregion
-
-    #region Unity Messages
-    private void OnTriggerStay(Collider _ground)
-    {
-        if (_ground.gameObject.CompareTag("Water"))
-            _isWater = true;
-        else if (_ground.gameObject.CompareTag("Soft"))
-            _isSoft = true;
-        else if (_ground.gameObject.CompareTag("Hard"))
-            _isHard = true;
-    }
-
-    private void OnTriggerExit(Collider _ground)
-    {
-        if (_ground.gameObject.CompareTag("Water"))
-            _isWater = false;
-        else if (_ground.gameObject.CompareTag("Soft"))
-            _isSoft = false;
-        else if (_ground.gameObject.CompareTag("Hard"))
-            _isHard = false;
+        else _movementStep = 1.0f;
     }
     #endregion
 }
---
+++ Assets/Scripts/Npc.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Npc : Interactable
+{
+    #region Variables
+    [Header("Npc Reference")]
+    [SerializeField] protected Animator _animator;
+    [SerializeField] protected NavMeshAgent _navMeshAgent;
+
+    [Header("Npc Stats")]
+    [SerializeField] protected float _wanderRange;
+    [SerializeField] protected float _wanderCooldown;
+    protected float _currentWanderCooldown;
+
+    [Header("Npc Talk")]
+    [SerializeField] protected GameObject _text;
+    [SerializeField] protected float _talkDuration;
+    protected float _talkTime;
+    protected bool _isTalking;
+    #endregion
+
+    #region Private Functions
+    protected virtual void Start()
+    {
+        Vector3 _randomPoint = RandomNavmeshPoint(transform.position, _wanderRange);
+        if (_randomPoint != Vector3.zero) _navMeshAgent.SetDestination(_randomPoint);
+    }
+
+    protected virtual void Update()
+    {
+        Animatorr();
+        Wander();
+        Talking();
+    }
+
+    protected void Animatorr()
+    {
+        if (_navMeshAgent.velocity.magnitude > 0.0f) _animator.SetBool("_isWalking", true);
+        else if (_navMeshAgent.velocity.magnitude <= 0.0f) _animator.SetBool("_isWalking", false);
+    }
+
+    protected void Wander()
+    {
+        if (_currentWanderCooldown < _wanderCooldown) _currentWanderCooldown += Time.deltaTime;
+        if (_currentWanderCooldown > _wanderCooldown) _currentWanderCooldown = 0.0f;
+        if (_navMeshAgent.stoppingDistance >= _navMeshAgent.remainingDistance && _currentWanderCooldown <= 0.0f)
+        {
+            Vector3 _randomPoint = RandomNavmeshPoint(transform.position, _wanderRange);
+            if (_randomPoint != Vector3.zero) _navMeshAgent.SetDestination(_randomPoint);
+        }
+    }
+
+    protected void Talking()
+    {
+        if (_isTalking)
+        {
+            _talkTime += Time.deltaTime;
+            if (_talkTime > _talkDuration)
+            {
+                _text.SetActive(false);
+                _talkTime = 0.0f;
+                _isTalking = false;
+            }
+        }
+    }
+
+    protected Vector3 RandomNavmeshPoint(Vector3 _center, float _range)
+    {
+        Vector3 _randomPoint = _center + Random.insideUnitSphere * _range;
+        NavMeshHit _hit;
+        if (NavMesh.SamplePosition(_randomPoint, out _hit, _range, NavMesh.AllAreas)) return _hit.position;
+        return Vector3.zero;
+    }
+
+    protected void OnCollisionEnter(Collision _hit)
+    {
+        if (_hit.gameObject.GetComponent<Door>()) _hit.gameObject.GetComponent<Door>().DoorHandle();
+    }
+    #endregion
+
+    #region Public Functions
+    public void Talk()
+    {
+        _text.SetActive(true);
+        _isTalking = true;
+        _talkTime = 0.0f;
+    }
+    #endregion
+}
