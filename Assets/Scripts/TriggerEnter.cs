737b679e58796a0d82b33c887086920254907b02
Nat <nathanaelangelotecson@gmai;.com>
2024-12-01 18:09:57 +0800

    commit

--- Assets/Scripts/TriggerEnter.cs
+++ Assets/Scripts/TriggerEnter.cs
@@ -6,13 +6,43 @@ using UnityEngine.Events;
 public class TriggerEnter : MonoBehaviour
 {
     #region Variables
+    private enum _ColliderDetection
+    {
+        Player,
+        Rigidbody,
+        Item,
+        Health,
+    }
+    [SerializeField] private _ColliderDetection _colliderDetection;
     [SerializeField] private UnityEvent _onTriggerEnterEvt;
     #endregion
 
     #region Unity Messages
     private void OnTriggerEnter(Collider _sense)
     {
-        if (_sense.gameObject.GetComponent<Player>()) _onTriggerEnterEvt.Invoke();
+        //Player
+        if (_colliderDetection == _ColliderDetection.Player && _sense.gameObject.GetComponent<Player>())
+        {
+            _onTriggerEnterEvt.Invoke();
+        }
+
+        //Rigidbody
+        else if (_colliderDetection == _ColliderDetection.Rigidbody && _sense.gameObject.GetComponent<Rigidbody>())
+        {
+            _onTriggerEnterEvt.Invoke();
+        }
+
+        //Item
+        else if (_colliderDetection == _ColliderDetection.Item && _sense.gameObject.GetComponent<Item>())
+        {
+            _onTriggerEnterEvt.Invoke();
+        }
+
+        //Health
+        else if (_colliderDetection == _ColliderDetection.Health && _sense.gameObject.GetComponent<Health>())
+        {
+            _onTriggerEnterEvt.Invoke();
+        }
     }
     #endregion
 }
