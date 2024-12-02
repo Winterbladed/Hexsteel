508aad2ee53110070151903927cd5f7273144721
Nat <nathanaelangelotecson@gmai;.com>
2024-12-02 13:13:08 +0800

    commit

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,10 +6,11 @@ public class Spawn : MonoBehaviour
     [SerializeField] protected Vector3 _addToVector;
     [SerializeField] protected GameObject[] _spawn;
     [SerializeField] private Transform _transform;
+    private bool _isSpawned;
     #endregion
 
     #region Public Functions
-    public void Spawning(int _amount)
+    public void SpawnMultipleRandomly(int _amount)
     {
         for (int i = 0; i < _amount; i++)
         {
@@ -17,7 +18,15 @@ public class Spawn : MonoBehaviour
         }
     }
 
-    public void Spawning1()
+    public void SpawnFirstMultiple(int _amount)
+    {
+        for (int i = 0; i < _amount; i++)
+        {
+            Instantiate(_spawn[0], transform.position + _addToVector, Quaternion.identity);
+        }
+    }
+
+    public void SpawnAllInArray()
     {
         foreach (GameObject _spawn in _spawn)
         {
@@ -25,19 +34,25 @@ public class Spawn : MonoBehaviour
         }
     }
 
-    public void Spawning2(int _index)
+    public void SpawnIndex(int _index)
     {
         Instantiate(_spawn[_index], transform.position + _addToVector, Quaternion.identity);
     }
 
-    public void Spawning3(int _index)
+    public void SpawnIndexOnTransform(int _index)
     {
         Instantiate(_spawn[_index], _transform.position, _transform.rotation);
     }
 
-    public void Spawning4(int _index)
+    public void SpawnIndexOnTransformAndRotationPlayer(int _index)
     {
         Instantiate(_spawn[_index], _transform.position + _addToVector, GetComponentInParent<Player>().transform.rotation);
+    }
+
+    public void SpawnOneRandomly()
+    {
+        if (!_isSpawned) Instantiate(_spawn[Random.Range(0, _spawn.Length)], _transform.position, _transform.rotation);
+        _isSpawned = true;
     }
     #endregion
 }
