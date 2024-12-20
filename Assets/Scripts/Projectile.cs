41c36b7fc211efe7bfca46fdc3254770b54ee10e
Nat <nathanaelangelotecson@gmai;.com>
2024-12-20 18:22:48 +0800

    commit

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,8 +6,7 @@ using UnityEngine.Events;
 public class Projectile : Damage
 {
     #region Variables
-    [Header("Projectile Stats")]
-    public Rigidbody _Rigidbody;
+    private Rigidbody _rigidbody;
     private TrailRenderer _trailRenderer;
     private Light _light;
     private enum ProjectileVector
@@ -16,6 +15,7 @@ public class Projectile : Damage
         UpperLeft, UpperRight, LowerRight, LowerLeft
     }
     private enum ProjectileType { Normal, Physics }
+    [Header("Projectile Stats")]
     [SerializeField] private ProjectileVector _projectileVector;
     [SerializeField] private ProjectileType _projectileType;
     [Range(0.0f, 10000.0f)]
@@ -33,25 +33,26 @@ public class Projectile : Damage
         base.Start();
         CriticalDamageChance();
         StatusChance();
-        DetermineVectorVelocity();
-        Destroy(gameObject, _projectileTimer);
+        _rigidbody = GetComponent<Rigidbody>();
         _trailRenderer = GetComponent<TrailRenderer>();
         _light = GetComponent<Light>();
+        DetermineVectorVelocity();
+        Destroy(gameObject, _projectileTimer);
     }
 
     private void DetermineVectorVelocity()
     {
-        if (_projectileVector == ProjectileVector.Straight) _Rigidbody.velocity = transform.forward * _projectileSpeed;
+        if (_projectileVector == ProjectileVector.Straight) _rigidbody.velocity = transform.forward * _projectileSpeed;
         else
         {
-            _Rigidbody.velocity += new Vector3
+            _rigidbody.velocity += new Vector3
             (
                 (_projectileVector == ProjectileVector.Left || _projectileVector == ProjectileVector.UpperLeft || _projectileVector == ProjectileVector.LowerLeft) ? -_projectileSpread
                     : ((_projectileVector == ProjectileVector.Right || _projectileVector == ProjectileVector.UpperRight || _projectileVector == ProjectileVector.LowerRight) ? _projectileSpread : 0),
                 (_projectileVector == ProjectileVector.Lower || _projectileVector == ProjectileVector.LowerLeft || _projectileVector == ProjectileVector.LowerRight) ? -_projectileSpread
                     : ((_projectileVector == ProjectileVector.Upper || _projectileVector == ProjectileVector.UpperLeft || _projectileVector == ProjectileVector.UpperRight) ? _projectileSpread : 0),
                 0
-            ) * _projectileSpeed; _Rigidbody.velocity += transform.forward * _projectileSpeed;
+            ) * _projectileSpeed; _rigidbody.velocity += transform.forward * _projectileSpeed;
         }
     }
     #endregion
@@ -61,7 +62,7 @@ public class Projectile : Damage
     {
         if (!_isHit)
         {
-            _Rigidbody.useGravity = true;
+            _rigidbody.useGravity = true;
             _trailRenderer.enabled = false;
             _light.enabled = false;
             _onHitEvt.Invoke(); DealDamage(_hit.gameObject);
