2b5e9102889a121d5691b1ab1ea9d0d7bc834ffa
Nat <nathanaelangelotecson@gmai;.com>
2024-12-27 03:47:43 +0800

    commit

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -32,7 +32,7 @@ public class Projectile : Damage
         CriticalDamageChance();
         StatusChance();
         DetermineVectorVelocity();
-        Destroy(gameObject, 3.0f);
+        Destroy(gameObject, 2.0f);
     }
 
     private void DetermineVectorVelocity()
